42a34fbc68258cccc03675b62f1371dfab403a72
Akash Singh <max.payne61@gmail.com>
2018-01-21 16:15:34 +0530

    ui changes

--- src/app/pages/admin-page/manage-student/student-marks-update/student-marks-update.component.ts
+++ src/app/pages/admin-page/manage-student/student-marks-update/student-marks-update.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-student-marks-update',
   templateUrl: './student-marks-update.component.html',
-  styleUrls: ['./student-marks-update.component.css']
 })
-export class StudentMarksUpdateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class StudentMarksUpdateComponent {
 
 }
